f0f7fb291849c5dd8e171823065b993c091b7bd8
Angel Montaño Vargas <angel.montano@ids.com.mx>
2023-09-02 12:05:02 -0600

    Se añade tw.macro

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './input.css'
+import GlobalStyles from './styles.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles/>
     <App />
   </React.StrictMode>,
 )
